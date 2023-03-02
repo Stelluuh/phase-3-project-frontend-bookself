8eccd996896c0116f6f7c9ca7124e40b0cfb59b1
stella <sftamesis13@gmail.com>
2023-03-02 14:55:25 -0800

    add components

---
+++ src/Components/Bookshelves.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bookshelves = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Bookshelves
---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../Stylesheets/Navbar.css'
+
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <nav>
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink to="/bookshelfs">Bookshelves</NavLink>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
