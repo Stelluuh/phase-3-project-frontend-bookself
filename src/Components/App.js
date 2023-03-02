4cf6334b660610acf1f706a9f99064bb6cbc9597
stella <sftamesis13@gmail.com>
2023-03-02 15:14:47 -0800

    fetch bookshelves from backend

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import '../App.css';
 
-function App() {
+const App = () => {
+  const [bookshelves, setBookshelves] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/bookshelfs')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
+
+
   return (
     <div className="App">
       <h1>App</h1>
--- src/Components/Bookshelf.js
+++ src/Components/Bookshelf.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Shelf = () => {
+const Bookshelf = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Shelf = () => {
   )
 }
 
-export default Shelf
+export default Bookshelf
