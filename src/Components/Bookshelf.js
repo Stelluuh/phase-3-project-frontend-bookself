a813e61cbd3d2bf9ee647c528991900bd6209cee
stella <sftamesis13@gmail.com>
2023-03-02 19:04:20 -0800

    add useEffect to retrieve all bookdetails from /books/:id

--- src/Components/Bookshelf.js
+++ src/Components/Bookshelf.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Bookshelf = () => {
-  const [books, setBooks] = useState([])
+  const [bookshelf, setBookshelf] = useState({
+    books: []
+  })
+  const params = useParams()
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/books/:${params.id}`)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
+  // console.log(bookshelf.books)
 
   return (
     <div>
-      <h3>Hello from books!</h3>
+      <h3>Hello from bookshelf!</h3>
     </div>
   )
 }
